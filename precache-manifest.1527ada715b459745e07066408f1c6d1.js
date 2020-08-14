5cb9e0fd6bec5c979fe654d3a4de192fccdd7a9d
creative6 <1228745275@qq.com>
2020-08-14 15:41:57 +0800

    Add files via upload

---
+++ precache-manifest.1527ada715b459745e07066408f1c6d1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2be0aa1a2192e7178c8787587b2db9c0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5bfd728909cf2a72d588",
+    "url": "./static/css/2.64c78a5b.chunk.css"
+  },
+  {
+    "revision": "d62189a42b74c554e6f5",
+    "url": "./static/css/main.d9134015.chunk.css"
+  },
+  {
+    "revision": "5bfd728909cf2a72d588",
+    "url": "./static/js/2.2be4516c.chunk.js"
+  },
+  {
+    "revision": "4431613bff3a45544ca7136bb52528bc",
+    "url": "./static/js/2.2be4516c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d62189a42b74c554e6f5",
+    "url": "./static/js/main.f7095ca5.chunk.js"
+  },
+  {
+    "revision": "5a6fe05e99d9b9cad124",
+    "url": "./static/js/runtime-main.74d55feb.js"
+  },
+  {
+    "revision": "63a50988eea349d1f07a9901330474f5",
+    "url": "./static/media/Mako-Regular-webfont.63a50988.ttf"
+  },
+  {
+    "revision": "365b0ce03d3c23dfa37f5b1e0ef92b87",
+    "url": "./static/media/loading.365b0ce0.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c7aeddd82ea350458b58ffa1279f0088.js"
+  "./precache-manifest.1527ada715b459745e07066408f1c6d1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
